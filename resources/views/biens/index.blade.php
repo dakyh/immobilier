8c58eb179b0ac90578ac74bd3bc2038cbd01b2fc
Thierno diallo <07j01ismaila@gmail.com>
2024-08-05 22:35:56 +0000

    fusion avec la version de khady + ajout des autres vues des biens

--- resources/views/biens/index.blade.php
+++ resources/views/biens/index.blade.php
@@ -23,60 +23,62 @@
     {{-- css biens --}}
     <link rel="stylesheet" href="{{ asset('css/biens/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/biens/color.css') }}">
+    <title>HALBI</title>
 
 </head>
 <body>
   <header class="header" id="header">
     <nav class="nav container">
-        <a href="#" class="nav__logo">
+        <a href="/" class="nav__logo">
             Holding Ahmad Loyal Business Invest <i class='bx bxs-home-alt-2'></i>
         </a>
 
-        <div class="nav__menu">
+        {{-- <div class="nav__menu">
             <ul class="nav__list">
                 <li class="nav__item">
-                    <a href="#home" class="nav__link active-link">
+                    <a href="/#home" class="nav__link active-link">
                         <i class='bx bx-home-alt-2' ></i>
                         <span>Accueil</span>
                     </a>
                 </li>
 
                 <li class="nav__item">
-                    <a href="#popular" class="nav__link">
+                    <a href="/#popular" class="nav__link">
                         <i class='bx bx-building-house' ></i>
                         <span>Résidences</span>
                     </a>
                 </li>
 
                 <li class="nav__item">
-                    <a href="#value" class="nav__link">
+                    <a href="/#value" class="nav__link">
                         <i class='bx bx-award' ></i>
                         <span>Services</span>
                     </a>
                 </li>
 
                 <li class="nav__item">
-                    <a href="#contact" class="nav__link">
+                    <a href="/#contact" class="nav__link">
                         <i class='bx bx-phone' ></i>
                         <span>Contact</span>
                     </a>
                 </li>
             </ul>
-        </div>
+        </div> --}}
 
         <!-- Theme change button -->
         <i class='bx bx-moon change-theme' id="theme-button" ></i>
 
-        <a href="#subscribe" class="button nav__button">
+        <a href="login" class="button nav__button">
             Mon espace
         </a>
     </nav>
 </header>
+
 <section class="home section" id="home">
   <div class="home__container container ">
       <div class="">
           <h1 class="home__title">
-              Découvrez <br> nos différents propriétées
+              Découvrez <br> nos différentes propriétées
           </h1>
           
 
